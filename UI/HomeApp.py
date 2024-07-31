85ba2e391afbf51d2a50c57499dffe63595f3032
Andrés Delgad <andresdelgadoc98@gmail.com>
2024-07-31 15:49:00 -0600

    test2

--- UI/HomeApp.py
+++ UI/HomeApp.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
 import threading
-from Functions.utils import request_sensor_data, load_sensors
+from Functions.utils import load_sensors
+from Functions.sensor_manager import SensorManager
 import datetime
 
 class HomeApp(tk.Frame):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.main = self.master
+        self.sensor_manager = SensorManager()  # Instancia del SensorManager
 
         self.create_widgets()
         self.update_sensor_info()
@@ -27,16 +29,21 @@ class HomeApp(tk.Frame):
         self.sensor_info.insert(tk.END, f"Timestamp: {timestamp}\n")
 
         for host, port in self.sensors:
-            sensor_data = request_sensor_data(host, port)
-            if sensor_data is None:
-                self.sensor_info.insert(tk.END, f"Error al recibir datos del sensor {host}:{port}\n")
-            else:
+            sensor_data = self.sensor_manager.request_data(host, port)
+            if "Error" in sensor_data or "Timeout" in sensor_data:
                 self.sensor_info.insert(tk.END, f"{sensor_data}\n")
+            else:
+                self.sensor_info.insert(tk.END, f"Sensor {host}:{port} data: {sensor_data}\n")
 
-        self.after(2000, self.update_sensor_info)
+        self.after(2000, self.update_sensor_info)  # Actualiza la información cada 2 segundos
+
+    def on_closing(self):
+        self.sensor_manager.close_all()
+        self.main.destroy()
 
 if __name__ == '__main__':
     root = tk.Tk()
     app = HomeApp(root)
+    root.protocol("WM_DELETE_WINDOW", app.on_closing)
     app.pack(fill='both', expand=True)
     root.mainloop()
